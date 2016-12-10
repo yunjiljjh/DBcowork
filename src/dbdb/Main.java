fd6081daf1c4302064ba4a7b93869cf7585e7a01
yunjiljjh <yunjiljjh@naver.com>
2016-12-10 23:44:56 +0900

    .
    .

--- src/dbdb/Main.java
+++ src/dbdb/Main.java
@@ -7,6 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
 
 public class Main {
 
@@ -15,12 +19,21 @@ public class Main {
 	private static final String DB_USER = "postgres";
 	private static final String DB_PASSWORD = "dddd";
 	
+	static Statement st;
+	static PreparedStatement preparedStmt;
+	static ResultSet rs;
+	static Properties connProps;
+	static Connection conn;
+	
+	private FileReader fr;
+	private BufferedReader br;
+	
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 
 		Class.forName(DB_DRIVER); //Driver class is loaded to memory
 		System.out.println("Driver loaded");
-		Properties connProps = new Properties();
+		connProps = new Properties();
 
 		/* Setting Connection Info */
 		connProps.setProperty("user", 		DB_USER);
@@ -28,36 +41,10 @@ public class Main {
 		System.out.println("connection set");
 
 		/* Connect! */
-		Connection conn = DriverManager.getConnection(DB_CONNECTION_URL, connProps);
+		conn = DriverManager.getConnection(DB_CONNECTION_URL, connProps);
 		System.out.println("connection success");
 		
-		Statement st = conn.createStatement();
-		
-		/* Create Table SQL */
-		String CreateTableSQL = "CREATE TABLE student_table " +
-								"(ID int, " +
-								"name varchar(20) not null, " +
-								"address varchar(50) not null," +
-								"department_ID int," +
-								"primary key (ID))";
-	 
-		st.executeUpdate(CreateTableSQL);
-				
-		/* Insert Row using Statement */
-		String InsertSQL_1 = "INSERT INTO student_table values(1, 'Brandt', 'addr1', 1)";
-		st.executeUpdate(InsertSQL_1);
-		
-		/* Insert Row using PreparedStatement */
-		String InsertSQL_2 = "INSERT INTO student_table (ID, name, address, department_ID) values(?, ?, ?, ?)";
-
-		PreparedStatement preparedStmt = conn.prepareStatement(InsertSQL_2);
-		preparedStmt.setInt(1, 2);
-		preparedStmt.setString(2, "Chavez");
-		preparedStmt.setString(3, "addr2");
-		preparedStmt.setInt(4, 2);
-		
-		preparedStmt.execute();
-		ResultSet rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
+		st = conn.createStatement();
 
 		System.out.println("============ RESULT ============");
 		while (rs.next()) {
@@ -91,16 +78,81 @@ public class Main {
 		st.executeUpdate(DeleteSQL);
 		
 		rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
+ 
+	}
 
-		System.out.println("============ RESULT ============");
-		while (rs.next()) {
-		    System.out.print("ID : " + rs.getString(1) + ", ");
-		    System.out.print("Name : " + rs.getString(2) + ", ");
-		    System.out.print("Address : " + rs.getString(3) + ", ");
-		    System.out.print("Department_ID : " + rs.getString(4));
-		    System.out.println();
-		}
+	public void mainMsg() throws IOException,FileNotFoundException, SQLException{
+		int input;
+        Scanner scan = new Scanner(System.in);
+		System.out.println("Please input the instruction number (1: Import from CSV, 2: Export to CSV, 3: Manipulate Data, 4: Exit) :");
+		input = scan.nextInt(); //get num from keyboard
 		
+		String tableName, fileName;
+		int option;
+		switch(input){
+			case 1: {System.out.println("[Import from CSV]");
+							System.out.println("Please specify the filename for table description : "); 
+							tableName = scan.nextLine();
+							System.out.println("Please specify the CSV filename : "); 
+							fileName = scan.nextLine();
+							createTable(tableName,fileName);
+							System.out.println("Table is newly created as described in the file.");
+							tableName=null;
+							fileName=null;
+							break;}
+			case 2: {System.out.println("[Export to CSV]");
+							System.out.println("Please specify the table name : "); 
+							tableName = scan.nextLine();
+							System.out.println("Please specify the CSV filename : "); 
+							fileName = scan.nextLine();
+							//exportTable(tableName,fileName ,infoArray)
+							System.out.println("Data export completed.");
+							tableName=null;
+							fileName=null;
+							break;}
+			case 3: {
+							System.out.println("[Manipulate Data]");
+							System.out.println("Please input the instruction number (1: Show Tables, 2: Describe Table, 3: Select, 4: Insert, 5: Delete, 6: Update, 7: Drop Table, 8: Back to main) : "); 
+							option=scan.nextInt();
+							switch(option){
+							case 1: { //showTables();
+											break;}
+							case 2: {//describeTable();
+											break;}
+							case 3: {//select();
+											break;}
+							case 4: {insertTuple();
+											break;}
+							case 5: {//delete();
+											break;}
+							case 6: {//update();
+											break;}
+							case 7: {dropTable();
+											break;}
+							case 8: {//back to main -> do nothing and return to the main home
+											break;}
+							}
+								break;}
+			case 4: {} break;
+			} //end of switch
+		}
+	
+	public void createTable(String tableName, String fileName) throws IOException, FileNotFoundException, SQLException {		
+		String CreateTableSQL = "CREATE TABLE tableName ";
+		String filePath;
+	    //filePath = fileName으로 경로 만들어서
+		fr=new FileReader(fileName);
+		br=new BufferedReader(fr);
+		String s = br.readLine();
+		String[] a = s.split(",");
+		
+		CreateTableSQL +=
+				"(" + a[0]+"변수타입"+", " + //읽어들인 파일로 나머지 string 만들고
+				"primary key ("+ "프라이머리 키 인 attribute"+"))";
+		st.executeUpdate(CreateTableSQL);
+		}
+	
+	public void dropTable() throws SQLException{
 		/* Drop table */
 		String DropTableSQL = "DROP TABLE student_table";
 		st.executeUpdate(DropTableSQL);
@@ -108,7 +160,28 @@ public class Main {
 		preparedStmt.close();
 		st.close();
 		rs.close();
- 
 	}
+	
+	public void insertTuple() throws SQLException{
+		
+		/* Insert Row using Statement 
+		 * 모든 정보가 한번에 다 있을 때*/
+		String InsertSQL_1 = "INSERT INTO student_table values(1, 'Brandt', 'addr1', 1)";
+		st.executeUpdate(InsertSQL_1);
+		
+		/* Insert Row using PreparedStatement
+		 * 정보를 하나씩 하나씩 넣을 때 */
+		String InsertSQL_2 = "INSERT INTO student_table (ID, name, address, department_ID) values(?, ?, ?, ?)";
 
+		preparedStmt = conn.prepareStatement(InsertSQL_2);
+		preparedStmt.setInt(1, 2);
+		preparedStmt.setString(2, "Chavez");
+		preparedStmt.setString(3, "addr2");
+		preparedStmt.setInt(4, 2);
+		
+		preparedStmt.execute();
+		
+		rs = st.executeQuery("SELECT ID, name, address, department_ID FROM student_table");
+	}
+	
 }
